a5762b428f238196e668c5873f88ef7bdff4d5ba
swapnilnarad2000 <swapniln@iitbhilai.ac.in>
2022-03-29 01:12:09 +0530

    Frontend: Form UI updated

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -2,19 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://practice.geeksforgeeks.org/topic-tags/"
+URL_ = "https://practice.geeksforgeeks.org/company-tags"
 page = requests.get(URL)
+page_ = requests.get(URL_)
 
 soup = BeautifulSoup(page.content, "html.parser")
-
+soup_ = BeautifulSoup(page_.content, "html.parser")
 results_1 = soup.find(id="dataStructures")
 results_2 = soup.find(id="algorithms")
 results_3 = soup.find(id="others")
-topics = results_1.find_all("b") + results_2.find_all("b") + results_3.find_all("b")
+results = soup_.find_all("b")
+# topics = results_1.find_all("b") + results_2.find_all("b") + results_3.find_all("b")
 
 
 list_topics = []
 
-for i in topics:
+for i in results:
     list_topics.append(i.text)
 
 print(list_topics)
+# print(results)
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
+var companies = ['Amazon', 'Microsoft', 'Flipkart', 'Google', 'Adobe', 'Samsung', 'Accolite', 'MakeMyTrip', 'Snapdeal', 'Paytm', 'Zoho', 'Walmart', 'Morgan Stanley', 'OYO Rooms', 'Goldman Sachs', 'FactSet', 'D-E-Shaw', 'SAP Labs', 'Ola Cabs', 'Hike', 'MAQ Software', 'Oracle', 'VMWare', 'Qualcomm', 'Intuit ', 'Cisco', 'Facebook', 'Visa', 'Directi', 'Linkedin', 'Payu', 'Yahoo', 'Wipro', 'Yatra.com', 'Belzabar', 'Salesforce', 'Housing.com', 'Teradata', 'Synopsys', 'BankBazaar', 'Codenation', 'Citrix', 'Mahindra Comviva', 'TCS', 'InMobi', '24*7 Innovation Labs', 'One97', 'Apple', 'Times Internet', 'Twitter', 'Veritas', 'PayPal', 'Tejas Network', 'Boomerang Commerce', 'Groupon', 'Nutanix', 'Brocade', 'InfoEdge', 'Myntra', 'ABCO', 'CouponDunia', 'Cognizant ', 'Atlassian', 'Lybrate', 'Amdocs', 'IgniteWorld ', 'OATS Systems', 'United Health Group', 'Opera', 'Oxigen Wallet', 'Juniper Networks', 'Nagarro', 'Quikr', 'Rockstand', 'TinyOwl', 'GreyOrange ', 'Streamoid Technologies', 'Infosys', 'MetLife', 'Moonfrog Labs', 'Philips', 'Drishti-Soft', 'GE', 'PropTiger', 'Polycom', 'Kritikal Solutions', 'Media.net ', 'BrowserStack', 'Zillious', 'Fab.com', 'Accenture', 'IBM', 'CarWale', 'Cadence India', 'Epic Systems', 'Grofers', 'Nvidia', 'Wooker', 'Monotype Solutions', 'Arcesium', 'Bloomberg', 'nearbuy', 'Sapient', 'Xome', 'Expedia', 'Tesco', 'Airtel', 'Citicorp', 'eBay', 'Netskope ', 'Kuliza', 'DE Shaw', 'Swiggy', 'Dell', 'Infinera', 'PlaySimple', 'Rivigo', 'Intel', 'Pubmatic', 'Vizury Interactive Solutions', 'Zycus', 'Jabong', 'Informatica', 'Unisys', 'Sprinklr', 'Medlife', 'Dunzo', 'Mobicip', 'Dailyhunt', 'National Instruments', 'Komli Media', 'Target Corporation', 'Junglee Games', 'Taxi4Sure', 'HSBC', 'Service Now', 'HCL', 'FreeCharge', 'Mallow Technologies', 'CGI', 'Zopper', 'redBus', 'KLA Tencor', 'Open Solutions', 'American Express', 'Practo', 'Bidgely', 'Uber', 'Yodlee Infotech', 'Code Brew', 'Cavisson System', 'HunanAsset', 'Motlay', 'Zomato', 'Freshokartz', 'Huawei', 'Knowlarity'];
+var list = document.getElementById('comapanyName');
+
+companies.forEach(function (item) {
+    var option = document.createElement('option');
+    option.value = item;
+    option.text = item;
+    list.appendChild(option);
+});
+
+
 let roundNumber = 1
 let questionNumber = 1
 let listQuestionByRound = [questionNumber];
