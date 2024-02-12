f6881df3c0cbf33a63b19d089f5dbb172b75c94e
swapnilnarad2000 <swapnilnarad2000@gmail.com>
2024-02-13 01:11:14 +0530

    Updated Image paths

--- static/assets/js/scripts.js
+++ static/assets/js/scripts.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function () {
     /*
         Background slideshow
     */
-    $('.top-content').backstretch("/static/assets/img/backgrounds/gymkhana.jpg");
+    $('.top-content').backstretch("static/assets/img/backgrounds/gymkhana.jpg");
 
     $('#top-navbar-1').on('shown.bs.collapse', function () {
         $('.top-content').backstretch("resize");
